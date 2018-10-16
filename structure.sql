4cb33e9de11494e9e8d376b3163ccc743865f8d0
Brian Stinar <brian@noventum.us>
2018-10-16 17:54:29 -0600

    Added stateURL to states, and cities, to schema.

--- structure.sql
+++ structure.sql
@@ -1,6 +1,17 @@
+DROP TABLE IF EXISTS states;
 CREATE TABLE states(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     stateName VARCHAR(15),
+    stateUrl VARCHAR(2083), # https://stackoverflow.com/questions/219569/best-database-field-type-for-a-url
     dateCreated TIMESTAMP,
-    deleted TIMESTAMP
+    deleted int # Problems with TIMESTAMP here...? 
+);
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    cityName VARCHAR(50),
+    cityUrl VARCHAR(2083), 
+    dateCreated TIMESTAMP, 
+    deleted int
 );
